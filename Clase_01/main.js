345e33deaf62341df8342841e2fafbf6ff020796
pgrandoli <pgrandoli@hotmail.com>
2023-04-14 10:05:45 -0300

    Se agrega Clase 02

---
+++ Clase_01/main.js
@@ -0,0 +1,15 @@
+//Clase 01
+//-----------------
+
+
+/* 
+let edad = 45;
+var nomnbre = "Pablo";
+const PI = 3.1416;
+
+console.log(edad); */
+
+let nombre = prompt("Ingrese su nombre");
+
+//alert(document.cookie);
+alert(nombre);
